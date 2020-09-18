72c0c8a32c0ab9732363681d33d98caefadb655e
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-18 14:37:20 -0700

    Request data from github and return correct data

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,40 @@
 const axios = require('axios');
 const config = require('../config.js');
+const request = require('request');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username, callback) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    method: 'GET',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
-  };
+    // transformResponse: (data) => {
+    //   // Do whatever you want to transform the data
 
+    //   return data;
+    // }]
+  };
+  var data = axios(options)
+    .then((response) => {
+      //only want certain fields of the data object
+      if(response.error) {
+        console.log(response.error)
+      }
+      callback(null, response.data);
+    });
+    // .catch((error) => {
+    //   if(error.response) {
+    //     callback(error.response);
+    //   }
+    // });
+    return data;
 }
 
 module.exports.getReposByUsername = getReposByUsername;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 let app = express();
 
+const getReposByUsername = require('../helpers/github.js')
+
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
@@ -14,6 +16,16 @@ app.post('/repos', function (req, res) {
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  //need to change from hardcoded to username from the req
+  var data = getReposByUsername.getReposByUsername('JessicaWildermuth', (error, data) => {
+    console.log(Array.isArray(data));
+    var userRepoData = []
+    data.forEach((repo) => {
+      userRepoData.push({owner: repo.owner.login, reponame: repo.name, stargazers: repo['stargazers_count'], lastUpdated: repo['updated_at'], forks: repo['forks_count'], url: repo['html_url'], description: repo.description});
+    })
+    console.log(userRepoData)
+    res.send(userRepoData);
+  });
 });
 
 let port = 1128;
